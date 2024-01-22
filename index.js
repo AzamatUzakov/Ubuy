76d9618fea468eeb5f975ec9285205aa8fa7f5a7
AzamatUzakov <azamatakmal053@gmail.com>
2024-01-22 16:50:45 +0500

    сделал до блока OFFER

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+let time = 3600
+let countDOWNel = document.querySelector('.time')
+
+setInterval(updateCountdown, 1000)
+
+
+function updateCountdown() {
+
+  const minet = Math.floor(time / 60)
+  let second = time % 60
+  second = second < 10 ? "0" + second : second
+  countDOWNel.innerHTML =`${minet}:${second}`
+  time--
+}
